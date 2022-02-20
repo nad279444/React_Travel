9039d42554936ceee5ab33663813ef8b0c31ef77
unknown <nad279444>
2022-02-20 06:50:47 -0800

    getting travel logs from nodejs api using useEffect hook

--- src/App.js
+++ src/App.js
@@ -18,10 +18,24 @@
 //   />;
 // }
 
-import * as React from 'react';
+import React ,{useState,useEffect }from 'react';
 import Map, {Marker} from 'react-map-gl';
 
 function App() {
+  const[placesInfo,setPlacesInfo] = useState([])
+
+  async function listLogs(){
+    const response = await fetch('http://localhost:1337/routes');
+    const data = await response.json();
+    setPlacesInfo(data);
+  }
+
+  useEffect(() => {
+     listLogs();
+     console.log(placesInfo)
+  
+  },[placesInfo])
+
   return <Map
     initialViewState={{
       longitude: -100,
@@ -32,7 +46,7 @@ function App() {
     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
   >
     <Marker longitude={-100} latitude={40} anchor="bottom" >
-      <img src="./pin.png" />
+      <img src="./pin.png " alt="marker" />
     </Marker>
   </Map>;
 }
