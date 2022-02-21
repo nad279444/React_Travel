5c0c8f0216b678d2ca83359498a65face75662e4
unknown <nad279444>
2022-02-21 07:58:08 -0800

    markers can get the location from my api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 
 import React ,{useState,useEffect }from 'react';
-import Map, {Marker} from 'react-map-gl';
+import Map, {Marker,Popup} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
   const[placesInfo,setPlacesInfo] = useState([]);
+  const [showPopup,setShowPopup] = useState({});
   const [viewState,setViewState] = useState({
     longitude: 1.0232,
     latitude: 7.9465,
@@ -37,18 +38,35 @@ function App() {
     style={{width: '100vw', height: '100vh'}}
     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
   >
-    {placesInfo.map(entry => (  
+    {placesInfo.map(entry => (
+      <>
       <Marker longitude={entry.longitude} latitude={entry.latitude} key={entry._id} >
-            <img 
+        <div onClick={() => setShowPopup({
+          [entry._id]:true,
+        })}>
+        <img 
             style={{width:24,height:24}}
             className='marker'
             src=" https://i.imgur.com/y0G5YTX.png"
             alt="marker" 
             />
+        </div>
+           
      </Marker>
-     )    
-    )}
-    
+     {showPopup[entry._id] && (
+      <Popup longitude={entry.longitude} latitude={entry.latitude}
+        anchor="top"
+        dynamicPosition={true}
+        onClose={() => setShowPopup(false)}>
+        <div>{entry.title}</div>
+      </Popup>)}
+     </>
+     ), 
+  
+    )
+   
+    }
+ 
   </Map>;
 }
 
